0892302b923c034f092db22bba07cd2d92f393c1
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-28 17:05:35 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -2,7 +2,7 @@ module FancyDance
   module InstanceMethods 
     
     def twirl 
-      "I'm twirling"
+      "I'm twirling!"
     end 
     
     def jump 
@@ -10,7 +10,7 @@ module FancyDance
     end 
     
     def pirouette 
-      "I'm doing a pirouette"
+      "I'm doing a pirouette."
     end 
     
     def take_a_bow 
